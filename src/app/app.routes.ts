65023f5b27b8e04a00762dcc7b935416d0dd00cc
krevetkot <ksenia.slonimskaya@yandex.ru>
2025-09-17 13:29:51 +0300

    routing to item page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {ShopComponent} from './pages/shop/shop.component';
 import {ItemComponent} from './pages/item/item.component';
+import {ProfileComponent} from './pages/profile/profile.component';
 
 export const routes: Routes = [
   {path: '', component: ShopComponent, pathMatch: 'full'},
-  {path: 'item/:id', component: ItemComponent}
+  {path: 'item/:id', component: ItemComponent},
+  {path: 'profile', component: ProfileComponent}
 ];
